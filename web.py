9d150a659dcd00c37c3431696e5b7be8363c09c9
u4ik <insighteuphoric@gmail.com>
2021-10-20 01:58:31 -0400

    ssl_functional/socket_wrapped/ command-line_arguement_functional

--- web.py
+++ web.py
@@ -1,53 +1,65 @@
 import pprint
 import socket
 import ssl
-# url  = "http://example.org/index.html"
+
+# ? Test URL
+# url = "http://example.org/index.html"
 
 def request(url):
-    ctx = ssl.create_default_context()
     s = socket.socket(
-
-    family=socket.AF_INET,
-    type=socket.SOCK_STREAM,
-    proto=socket.IPPROTO_TCP,
+        family=socket.AF_INET,
+        type=socket.SOCK_STREAM,
+        proto=socket.IPPROTO_TCP,
     )
 
-
-    assert url.startswith("http://")
-    url = url[len("http://"):]
+    scheme, url = url.split("://", 1)
+    hostname = url.split("/",1)[0]
+    # print(hostname)
+    assert scheme in ["http", "https"], \
+        "Unknown scheme {}".format(scheme)
+    port = 80 if scheme == "http" else 443
+    print({port: port, scheme: scheme})
     host, path = url.split("/", 1)
     path = "/" + path
-    # print({"host":host, "path":path})
+    print({"host":host, "path":path, "url": url, "hostname": hostname})
+    if scheme == "https":
+        ctx = ssl.create_default_context()
+        s = ctx.wrap_socket(s, server_hostname=host)
 
-    s.connect((host, 80))
+    if ":" in host:
+        host, port = host.split(":", 1)
+        port = int(port)
+
+    s.connect((hostname, port))
     # Implementing the "b" before the string specifies that there are bytes of information being sent, instead of the raw text.
     # This send request returns back to us "47" which are the amount of bytes that we've sent
-    print("Sent Bytes:",s.send(b"GET /index.html HTTP/1.0\r\n" + b"Host: example.org\r\n\r\n"))
+    print("Sent Bytes:", s.send(
+        b"GET /index.html HTTP/1.0\r\n" + b"Host: example.org\r\n\r\n"))
 
     # makefile returns a file-like object containing every byte receieved from the server. Turning those bytes into a string using utf8 encoding. Also removing HTTP's weird line endings.
     response = s.makefile("r", encoding="utf8", newline="\r\n")
     statusline = response.readline()
     #print("statusline", statusline)
     version, status, explanation = statusline.split(" ", 2)
-    assert status == "200", "{}:{}".format(status,explanation)
-    #print({"version":version, "status":status,"explanation":explanation})
+    assert status == "200", "{}:{}".format(status, explanation)
+    print({"version":version, "status":status,"explanation":explanation})
     headers = {}
     while True:
         line = response.readline()
-        if line == "\r\n": break
+        if line == "\r\n":
+            break
         print(line)
-        header, value = line.split(":",1)
+        header, value = line.split(":", 1)
         headers[header.lower()] = value.strip()
-    #print(headers)
-    
+    # print(headers)
+
     body = response.read()
     s.close()
-    return headers,body
-    
+    return headers, body
 
 def displayBody(body):
-    #print(body)
-    #Print info between the <> brackets in the response body.
+    # print(body)
+    # Print info between the <> brackets in the response body.
     in_angle = False
     for c in body:
         if c == "<" or c == "{":
@@ -55,16 +67,20 @@ def displayBody(body):
         elif c == ">" or c == "}":
             in_angle = False
         elif not in_angle:
-            print(c,end="")
+  
+            print(c, end="")
 
 
 def load(url):
     headers, body = request(url)
-    #print(headers)
+    # print(headers)
     displayBody(body)
 
+# ? Test Load
+# load(url)
+
+# ? Mimic a main function that will execute from the command line, this will allow the execution of this script with a URL passed in as an arg.
 if __name__ == "__main__":
     import sys
     if sys.argv[1]:
         load(sys.argv[1])
-    
