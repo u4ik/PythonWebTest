3b2c4ddd43db56fa0ddec8713cc84b783b3cae23
u4ik <insighteuphoric@gmail.com>
2021-10-19 23:12:54 -0400

    comments

--- web.py
+++ web.py
@@ -17,3 +17,6 @@ s.connect(("example.org", 80))
 # Implementing the "b" before the string specifies that there are bytes of information being sent, instead of the raw text.
 # This send request returns back to us "47" which are the amount of bytes that we've sent
 print(s.send(b"GET /index.html HTTP/1.0\r\n" + b"Host: example.org\r\n\r\n"))
+
+# makefile returns a file-like object containing every byte receieved from the server. Turning those bytes into a string using utf8 encoding. Also removing HTTP's weird line endings.
+response = s.makefile("r", encoding="utf8", newline="\r\n")
